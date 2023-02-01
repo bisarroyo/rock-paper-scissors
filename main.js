3d01f6f372f665e8c3e8ee308d95136792fac1bc
Bismark Arroyo <bis.arroyo@gmail.com>
2023-01-31 22:33:52 -0600

    Added logic and styles from previous project

--- main.js
+++ main.js
@@ -1,23 +1,62 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let stone = document.getElementById("stone").addEventListener("click", () => game(1));
+let paper = document.getElementById("paper").addEventListener("click", () => game(2));
+let scissors = document.getElementById("scissors").addEventListener("click", () => game(3));
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const piedra = 1;
+const papel = 2;
+const tijera = 3;
 
-setupCounter(document.querySelector('#counter'))
+//storage number of times the player win draw or loss.
+let win = [];
+let draw = [];
+let loss = [];
+
+const resultColor = document.querySelector(".machine-area__selection");
+
+const writeHTML = (result, icon, iconResult, color) =>
+{
+    document.getElementById("resultado").innerHTML = result;
+    document.getElementById("machine").innerHTML = icon;
+    document.getElementById("result-game").innerHTML = iconResult;
+    resultColor.className = `machine-area__selection ${color}`;
+};
+
+function game(user)
+{
+    let maquina = Math.floor(Math.random() * 3 + 1);
+    let select;
+    let result;
+    if (maquina === 1){
+        select = "<i class='fas fa-hand-rock icon-machine'></i>";
+        result = "<p id='result-game'>Piedra</p>";
+    }else if (maquina === 2){
+        select = "<i class='fas fa-hand-paper icon-machine'></i>";
+        result = "<p id='result-game'>Papel</p>";
+    }else {
+        select = "<i class='fas fa-hand-scissors icon-machine'></i>";
+        result = "<p id='result-game'>Tijera</p>";
+    }
+    if (user === maquina)
+    {
+        writeHTML("Has empatado", select, result, "yellow");
+        draw.push(1);
+    }else if (
+        (user === piedra && maquina === tijera) ||
+        (user === papel && maquina === piedra) ||
+        (user === tijera && maquina === papel)
+        )
+    {
+        writeHTML("Has ganado", select, result, "green");
+        win.push(1);
+    }else
+    {
+        writeHTML("Has perdido", select, result, "red");
+        loss.push(1);
+    }
+    document.getElementById("win-result").innerHTML = win.length;
+    document.getElementById("loss-result").innerHTML = loss.length;
+    document.getElementById("draw-result").innerHTML = draw.length;
+
+    (win.length === 3) ? alert("Has ganado a la máquina") : console.log("sigue jugando");
+    (loss.length === 3) ? alert("Más suerte para la próxima") : console.log("sigue jugando");
+}
