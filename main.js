34a0fdc8ede32a57c44870719edc9dc0efef8c0f
Bismark Arroyo <bis.arroyo@gmail.com>
2023-01-31 23:17:33 -0600

    Fixed logic code functions

--- main.js
+++ main.js
@@ -1,62 +1,80 @@
-let stone = document.getElementById("stone").addEventListener("click", () => game(1));
-let paper = document.getElementById("paper").addEventListener("click", () => game(2));
-let scissors = document.getElementById("scissors").addEventListener("click", () => game(3));
+document.getElementById('stone').addEventListener('click', () => gameLogic(1))
+document.getElementById('paper').addEventListener('click', () => gameLogic(2))
+document.getElementById('scissors').addEventListener('click', () => gameLogic(3))
 
-const piedra = 1;
-const papel = 2;
-const tijera = 3;
+const rock = 1
+const paper = 2
+const scissor = 3
 
-//storage number of times the player win draw or loss.
-let win = [];
-let draw = [];
-let loss = [];
+// storage number of times the player win draw or loss.
+let win = []
+let draw = []
+let loss = []
 
-const resultColor = document.querySelector(".machine-area__selection");
+const resultColor = document.querySelector('.machine-area__selection')
 
-const writeHTML = (result, icon, iconResult, color) =>
-{
-    document.getElementById("resultado").innerHTML = result;
-    document.getElementById("machine").innerHTML = icon;
-    document.getElementById("result-game").innerHTML = iconResult;
-    resultColor.className = `machine-area__selection ${color}`;
-};
+const writeHTML = ({ resultText, iconMachine, iconResult, color }) => {
+  document.getElementById('resultado').innerHTML = resultText
+  document.getElementById('machine').innerHTML = iconMachine
+  document.getElementById('result-game').innerHTML = iconResult
+  resultColor.className = `machine-area__selection ${color}`
+}
 
-function game(user)
-{
-    let maquina = Math.floor(Math.random() * 3 + 1);
-    let select;
-    let result;
-    if (maquina === 1){
-        select = "<i class='fas fa-hand-rock icon-machine'></i>";
-        result = "<p id='result-game'>Piedra</p>";
-    }else if (maquina === 2){
-        select = "<i class='fas fa-hand-paper icon-machine'></i>";
-        result = "<p id='result-game'>Papel</p>";
-    }else {
-        select = "<i class='fas fa-hand-scissors icon-machine'></i>";
-        result = "<p id='result-game'>Tijera</p>";
-    }
-    if (user === maquina)
-    {
-        writeHTML("Has empatado", select, result, "yellow");
-        draw.push(1);
-    }else if (
-        (user === piedra && maquina === tijera) ||
-        (user === papel && maquina === piedra) ||
-        (user === tijera && maquina === papel)
-        )
-    {
-        writeHTML("Has ganado", select, result, "green");
-        win.push(1);
-    }else
-    {
-        writeHTML("Has perdido", select, result, "red");
-        loss.push(1);
-    }
-    document.getElementById("win-result").innerHTML = win.length;
-    document.getElementById("loss-result").innerHTML = loss.length;
-    document.getElementById("draw-result").innerHTML = draw.length;
+const resetGame = () => {
+  win = []
+  draw = []
+  loss = []
+}
 
-    (win.length === 3) ? alert("Has ganado a la máquina") : console.log("sigue jugando");
-    (loss.length === 3) ? alert("Más suerte para la próxima") : console.log("sigue jugando");
+const machineSelection = () => {
+  // return a ramdon selection beetwen 1 - 3
+  const machine = Math.floor(Math.random() * 3 + 1)
+  let select
+  let result
+  if (machine === 1) {
+    select = "<i class='fas fa-hand-rock icon-machine'></i>"
+    result = "<p id='result-game'>Rock</p>"
+  } else if (machine === 2) {
+    select = "<i class='fas fa-hand-paper icon-machine'></i>"
+    result = "<p id='result-game'>Paper</p>"
+  } else {
+    select = "<i class='fas fa-hand-scissors icon-machine'></i>"
+    result = "<p id='result-game'>Scissor</p>"
+  }
+  return {
+    select,
+    result,
+    machine
+  }
+}
+
+function gameLogic (user) {
+  const { select, result, machine } = machineSelection()
+  if (user === machine) {
+    writeHTML({ resultText: 'You have tied', iconMachine: select, iconResult: result, color: 'yellow' })
+    draw.push(1)
+  } else if (
+    (user === rock && machine === scissor) ||
+    (user === paper && machine === rock) ||
+    (user === scissor && machine === paper)
+  ) {
+    writeHTML({ resultText: 'You have Won', iconMachine: select, iconResult: result, color: 'green' })
+    win.push(1)
+  } else {
+    writeHTML({ resultText: 'You have lost', iconMachine: select, iconResult: result, color: 'red' })
+    loss.push(1)
+  }
+  document.getElementById('win-result').innerHTML = win.length
+  document.getElementById('loss-result').innerHTML = loss.length
+  document.getElementById('draw-result').innerHTML = draw.length
+
+  if (win.length === 3) {
+    resetGame()
+    alert('Has ganado a la máquina')
+  } else if (loss.length === 3) {
+    resetGame()
+    alert('Más suerte para la próxima')
+  } else {
+    console.log('sigue jugando')
+  }
 }
